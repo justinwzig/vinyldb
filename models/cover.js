37f9b16c6553c79f7221aee2b484ae557b62fd38
Mutable Devotions <4504400+justinwzig@users.noreply.github.com>
2024-05-25 14:16:58 -0500

    add models back

---
+++ models/cover.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const { DateTime } = require("luxon"); // for date handling
+
+const Schema = mongoose.Schema;
+
+const GenericSchema = new Schema({
+  // Schema Versioning Pattern
+  schema_version: { type: Number, required: true, default: 1 },
+  // Document Versioning Pattern
+  revision: { type: Number, required: true, default: 1 },
+  // Date created
+  date_created: { type: Date, required: true },
+  // Date Modified
+  date_modified: { type: Date },
+  // Status
+  status: { type: String },
+
+  image_url: {
+    type: String,
+    required: true,
+    default: "/assets/default.png"
+  },
+});
+
+// Virtual for this generic instance URL.
+GenericSchema.virtual("url").get(function () {
+  return "/db/generic/" + this._id;
+});
+
+// Export model.
+module.exports = mongoose.model("Generic", GenericSchema);
---
+++ models/style.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const { DateTime } = require("luxon"); // for date handling
+
+const Schema = mongoose.Schema;
+
+const StyleSchema = new Schema({
+  // Schema Versioning Pattern
+  schema_version: { type: Number, required: true, default: 1 },
+  // Document Versioning Pattern
+  revision: { type: Number, required: true, default: 1 },
+  // Date created
+  date_created: { type: Date, required: true },
+  // Date Modified
+  date_modified: { type: Date },
+  // Status
+  status: { type: String },
+
+  name: {
+    type: String,
+    required: true
+  },
+  desc: {
+    type: String,
+  },
+});
+
+// Virtual for this style instance URL.
+StyleSchema.virtual("url").get(function () {
+  return "/db/style/" + this._id;
+});
+
+// Export model.
+module.exports = mongoose.model("Style", StyleSchema);
